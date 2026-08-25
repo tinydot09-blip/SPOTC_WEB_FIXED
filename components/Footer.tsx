bb242453d0e445187c5affb814e9a3c5e4e8ac5f
Spotc <tinydot09@gmail.com>
2026-08-25 20:39:10 +0530

    Add SPOTC logo to header and footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -12,7 +12,7 @@ export default function Footer() {
             aria-label="SPOTC Home"
           >
             <img
-              src="/images/web%20logo%20color%footer.png"
+              src="/images/web-logo-color-footer.png"
               alt="Spotc.in"
               className="spotc-footer-logo"
             />
