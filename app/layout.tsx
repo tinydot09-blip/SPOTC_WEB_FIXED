d127973bb113e276f19f46c90f9bb12513f44753
Spotc <tinydot09@gmail.com>
2026-09-10 21:02:51 +0530

    Remove Meta Pixel to improve site performance

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,15 +5,11 @@ import './globals.css';
 
 import { LanguageProvider } from '@/components/LanguageProvider';
 import GoogleAnalyticsPageView from '@/components/GoogleAnalyticsPageView';
-import MetaPixelPageView from '@/components/MetaPixelPageView';
 import NavigationLoader from '@/components/NavigationLoader';
 import RouteShell from '@/components/RouteShell';
 
 const GA_MEASUREMENT_ID =
   'G-YLJ3YNCN2C';
-
-const META_PIXEL_ID =
-  '816016971570677';
 
 const SITE_URL =
   'https://www.spotc.in';
@@ -247,68 +243,6 @@ export default function RootLayout({
         </Script>
 
         {/* =========================
-            META PIXEL
-            Delayed until browser load
-        ========================= */}
-
-        <Script
-          id="meta-pixel"
-          strategy="lazyOnload"
-        >
-          {`
-            !function(f,b,e,v,n,t,s)
-            {
-              if(f.fbq)return;
-
-              n=f.fbq=function(){
-                n.callMethod
-                  ? n.callMethod.apply(
-                      n,
-                      arguments
-                    )
-                  : n.queue.push(
-                      arguments
-                    );
-              };
-
-              if(!f._fbq)
-                f._fbq=n;
-
-              n.push=n;
-              n.loaded=!0;
-              n.version='2.0';
-              n.queue=[];
-
-              t=b.createElement(e);
-              t.async=!0;
-              t.src=v;
-
-              s=b.getElementsByTagName(e)[0];
-
-              s.parentNode.insertBefore(
-                t,
-                s
-              );
-            }(
-              window,
-              document,
-              'script',
-              'https://connect.facebook.net/en_US/fbevents.js'
-            );
-
-            fbq(
-              'init',
-              '${META_PIXEL_ID}'
-            );
-
-            fbq(
-              'track',
-              'PageView'
-            );
-          `}
-        </Script>
-
-        {/* =========================
             ORGANIZATION JSON-LD
         ========================= */}
 
@@ -344,22 +278,7 @@ export default function RootLayout({
       </head>
 
       <body>
-        {/* Meta Pixel fallback */}
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{
-              display: 'none',
-            }}
-            src={`https://www.facebook.com/tr?id=${META_PIXEL_ID}&ev=PageView&noscript=1`}
-            alt=""
-          />
-        </noscript>
-
         <GoogleAnalyticsPageView />
-
-        <MetaPixelPageView />
 
         <LanguageProvider>
           <NavigationLoader />
